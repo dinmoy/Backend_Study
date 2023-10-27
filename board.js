cadbe0174c111460bab748f1bc4191739a2ab1e4
dinmoy <imhyojin614@gmail.com>
2023-10-27 15:58:03 +0900

    [Feat] 게시판 POST 게시글 추가 구현

--- board.js
+++ board.js
@@ -13,6 +13,17 @@ const pool=mysql.createPool({ //커넥션을 만들어놓고 쓸 수 있는
         database: 'hozu'
 })
 
+app.post("/posts",(req,res)=>{
+    //여기서 게시글 생성과 관련된 작업 진행
+    pool.query(
+        "INSERT INTO post(title,author,createdAt,content) VALUES(?,?,now(),?)",
+        [req.body.title,req.body.author,req.body.content],
+        (err,rows,fields)=>{
+            if(err) res.status(400).json({result: err})
+            else res.json({result:"ok"})
+        }
+    )
+})
 app.listen(port,() =>{
     console.log(`Example app listening on port ${port}`)
 })
