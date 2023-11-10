bc0cb78d3c897922aca0b366a9a1a9d40a43d586
dinmoy <imhyojin614@gmail.com>
2023-11-10 17:27:32 +0900

    [Feat] 특정 유저 정보 가져오기

--- user.js
+++ user.js
@@ -24,3 +24,17 @@ app.post("/posts",(req,res)=>{
     )
 })
 
+app.get("/posts",(req,res)=>{
+    pool.query("SELECT * FROM user",(err,rows,fields)=>{
+        res.json({result: rows })
+    })
+})
+
+app.get("/posts/:id",(req,res)=>{
+    const id=req.params.id
+    pool.query("SELECT * FROM user WHERE id = ?",[id],(err,rows.fields)=>{
+        if(rows.length===0) res.send({result:null})
+        else res.json({result: rows[0]})
+    })
+})
+
